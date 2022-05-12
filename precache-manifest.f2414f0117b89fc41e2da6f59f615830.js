ff1091273d9654d70b0d3eae1a2bd1e91b7df1e2
John Harper <john.rr.harper@gmail.com>
2022-05-12 09:18:56 +0100

    Updates

---
+++ precache-manifest.f2414f0117b89fc41e2da6f59f615830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc48fb9b625256ab8afac7794d75fb5c",
+    "url": "/board_gaming/index.html"
+  },
+  {
+    "revision": "0630727b24f052119fa5",
+    "url": "/board_gaming/static/css/main.72fcfc05.chunk.css"
+  },
+  {
+    "revision": "a9d58d59c710822a1e38",
+    "url": "/board_gaming/static/js/2.d0d8643b.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/board_gaming/static/js/2.d0d8643b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a832ccb577518379ef0e",
+    "url": "/board_gaming/static/js/3.24c0116a.chunk.js"
+  },
+  {
+    "revision": "0630727b24f052119fa5",
+    "url": "/board_gaming/static/js/main.9ec9a8d1.chunk.js"
+  },
+  {
+    "revision": "a35c79de22d9014cdc50",
+    "url": "/board_gaming/static/js/runtime-main.d20e8d8c.js"
+  }
+]);
